f9cd2761e10bc0a6a00faa68df95090e44f73fae
Alice Fabre <51875307+Alice-F@users.noreply.github.com>
2021-12-17 14:58:05 +0100

    Revert "add visit model and create visit before #home and #strategyand"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,7 @@
 class PagesController < ApplicationController
-  before_action :create_visit, only: %i[home strategyand]
-
   def home
   end
 
   def strategyand
-  end
-
-  private
-
-  def create_visit
-    Visit.create(controller: params[:controller], action: params[:action])
   end
 end
